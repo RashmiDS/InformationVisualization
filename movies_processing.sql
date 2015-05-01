6e6cc0fa6047c89f89ca7fd0a056e8147f464584
RashmiDS <rdladdha@usfca.edu>
2015-05-01 14:24:27 -0700

    HW4

---
+++ movies_processing.sql
@@ -0,0 +1,6 @@
+﻿select * from movies limit 10;
+
+select "year",sum("Action") as Action, sum("Comedy") as Comedy,sum("Animation") as Animation,
+sum("Drama") as Drama,sum("Documentary") as Documentary, sum("Romance") as Romance,
+sum("Short") as Short from movies
+group by "year";
